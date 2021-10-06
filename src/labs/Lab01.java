f5bfe15f0a9872bab66ce5e3ca83f80bc867fee4
rhunter-NTatC <rhunter@newtech.coppellisd.com>
2021-10-06 14:03:40 -0500

    completed labs

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Lab01 {
 
-	public static String name = "";
+	public static String name = "Robert";
 	
 	public static void main(String[] args) {
 		
-		problem1();
-		problem2();
+//		problem1();
+//		problem2();
 		problem3();
 		
 		
@@ -20,7 +20,26 @@ public class Lab01 {
 	
 	public static void problem1() {
 		
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.print("Enter an integer: ");
+		int input = inKey.nextInt();
+		
+		if (input == 6) {
+			System.out.println("Five, six, pickup sticks!");
+		}
+		else if (input < 0) {
+			System.out.println("That’s so negative of you!");
+		}
+		else if (input >= 13 && input <= 19) {
+			System.out.println("Oh the teenage years...");
+		}
+		else if (input >= 20 && input % 2 == 0) {
+			System.out.println("What about the odds?");
+		}
+		else {
+			System.out.println("I haven't found that one yet.");
+		}
 		
 		
 	}
@@ -31,7 +50,17 @@ public class Lab01 {
 	
 	public static void problem2() {
 		
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.print("Do you know my name? ");
+		String name1 = inKey.nextLine();
+		
+		if (name.equals(name1)) {
+			System.out.println("What can I do for you?");
+		}
+		else {
+			System.out.println("You don't even know my name. We're THROUGH!!");
+		}
 		
 		
 	}
@@ -41,7 +70,21 @@ public class Lab01 {
 	
 	public static void problem3() {
 		
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.print("How many boards did you get? ");
+		int boards = inKey.nextInt();
+		System.out.print("How many boxes of nails did you get? ");
+		int nails = inKey.nextInt();
+		
+		int nailsNeeded = (int) Math.ceil(boards / 9.0);
+		
+		if (nails >= nailsNeeded) {
+			System.out.println("You have enough nails.");
+		}
+		else {
+			System.out.println("You need " + (nailsNeeded - nails) + " more nail boxes.");
+		}
 		
 	
 	}
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -18,8 +18,26 @@ public class Lab02 {
 	
 	public static void problem1() {
 		
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.print("Enter a number from 0 to 100: ");
+		int num = inKey.nextInt();
 		
+		if (num >= 90) {
+			System.out.println("That's an A");
+		}
+		else if (num >= 80) {
+			System.out.println("That's a B");
+		} 
+		else if (num >= 70) {
+			System.out.println("That's a C");
+		} 
+		else if (num >= 60) {
+			System.out.println("That's a D");
+		} 
+		else{
+			System.out.println("That's a F");
+		}
 		
 	}
 	
@@ -27,7 +45,31 @@ public class Lab02 {
 	
 
 	public static void problem2() {
+
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.println("Enter 3 angles.");
+		System.out.print("Angle #1: ");
+		int angle1 = inKey.nextInt();
+		System.out.print("Angle #2: ");
+		int angle2 = inKey.nextInt();
+		System.out.print("Angle #3: ");
+		int angle3 = inKey.nextInt();
+		
+		
+		
+		if (angle1 + angle2 + angle3 != 180) {
+			System.out.println("Those don't make a triangle.");
+		}
+		else if (angle1 == angle2 && angle1 == angle3) {
+			System.out.println("That's an equilateral triangle.");
+		}
+		else if (angle1 == angle2 || angle1 == angle3 || angle2 == angle3) {
+			System.out.println("That's an isosceles triangle.");
+		}
+		else {
+			System.out.println("That's a scalene triangle.");
+		}
 		
 		
 		
@@ -38,7 +80,53 @@ public class Lab02 {
 	
 	public static void problem3() {
 
+		Scanner inKey = new Scanner(System.in);
 		
+		System.out.println("Enter the psi for each tire.");
+		System.out.print("Driver's Front Tire: ");
+		double driverFront = inKey.nextDouble();
+		System.out.print("Driver's Rear Tire: ");
+		double driverRear = inKey.nextDouble();
+		System.out.print("Passenger's Front Tire: ");
+		double passengerFront = inKey.nextDouble();
+		System.out.print("Passenger's Rear Tire: ");
+		double passengerRear = inKey.nextDouble();
+		
+		boolean tiresFine = true;
+		
+		if (driverFront < 32) {
+			System.out.println("Warning: Driver's Front Tire Low!!");
+			tiresFine = false;
+		}
+		
+		if (driverRear < 32) {
+			System.out.println("Warning: Driver's Rear Tire Low!!");
+			tiresFine = false;
+		}
+		
+		if (passengerFront < 32) {
+			System.out.println("Warning: Passenger's Front Tire Low!!");
+			tiresFine = false;
+		}	
+		
+		if (passengerRear < 32) {
+			System.out.println("Warning: Passenger's Rear Tire Low!!");
+			tiresFine = false;
+		}
+		
+		if (Math.abs(driverFront - driverRear) > 4
+				|| Math.abs(driverFront - passengerFront) > 4
+				|| Math.abs(driverFront - passengerRear) > 4
+				|| Math.abs(driverRear - passengerFront) > 4
+				|| Math.abs(driverRear - passengerFront) > 4
+				|| Math.abs(passengerFront - passengerRear) > 4) {
+			System.out.println("Warning: Tire Pressure Unever!!");
+			tiresFine = false;
+		}
+		
+		if (tiresFine) {
+			System.out.println("Tires are fine");
+		}
 		
 		
 	}
@@ -48,8 +136,20 @@ public class Lab02 {
 
 	public static void problem4() {
 
+		Scanner inKey = new Scanner(System.in);
 		
-
+		System.out.print("Enter a positive integer: ");
+		int num = inKey.nextInt();
+		
+		int onesPlace = num % 10;
+		
+		num -= onesPlace;
+		
+		if (onesPlace >= 5) {
+			num += 10;
+		}
+		
+		System.out.println("Rounded to the nearest 10: " + num);
 		
 	}
 	
